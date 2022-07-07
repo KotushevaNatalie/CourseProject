0f9da2218645175cfff8edcca51c1eea5686b6c3
KotushevaNatalie <Kotushevanatalie@gmail.com>
2022-07-08 00:46:35 +0300

    Registration

--- src/main/java/com/example/courseproject/Controller.java
+++ src/main/java/com/example/courseproject/Controller.java
@@ -1,14 +1,55 @@
 package com.example.courseproject;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authorized;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button registration;
+
+    @FXML
+    void initialize() {
+        registration.setOnAction(actionEvent -> {registration.getScene().getWindow().hide();
+            FXMLLoader fxmloader = new FXMLLoader();
+            fxmloader.setLocation(getClass().getResource("/com/example/courseproject/registr.fxml"));
+
+            try {
+                fxmloader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent parent = fxmloader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(parent));
+            stage.showAndWait();
+        });
+
     }
+
 }
--- src/main/java/com/example/courseproject/RegistrController.java
+++ src/main/java/com/example/courseproject/RegistrController.java
@@ -1,4 +1,63 @@
 package com.example.courseproject;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class RegistrController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField lastname;
+
+    @FXML
+    private TextField loginr;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private PasswordField passwordr;
+
+    @FXML
+    private Button registration;
+
+    @FXML
+    private TextField secondname;
+
+    @FXML
+    void initialize() {
+        registration.setOnAction(actionEvent -> {
+            registration.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/example/courseproject/hello-view.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent p = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(p));
+            stage.showAndWait();
+        });
+    }
 }
+
+
